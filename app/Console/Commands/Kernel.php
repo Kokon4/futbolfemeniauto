1300cbf5d37407da9c99aeb67727711b66306179
Kokon4 <kicodbz1@gmail.com>
2025-06-16 11:47:25 +0200

    Kernel Nacho

--- app/Console/Commands/Kernel.php
+++ app/Console/Commands/Kernel.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 
 namespace App\Console;
 
@@ -7,9 +7,17 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    protected $commands = [
+        \App\Console\Commands\EnviarCalendariArbitres::class,
+    ];
+
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        // Run the command on January 1st at 00:00
+        $schedule->command('calendari:enviar-arbitres')
+                 ->yearly()
+                 ->on('1/1')
+                 ->at('00:00');
     }
 
     protected function commands()
